bc408db56ff9cbed5bf7272dbda63d14d6ff5762
marceloarmando <mcuellarr98@gmail.com>
2020-04-20 21:00:01 -0500

    Agregar archivos de proyecto.

---
+++ Lab_3/Lab_3/RelativeLayoutDemo.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab_3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RelativeLayoutDemo : ContentPage
+    {
+        public RelativeLayoutDemo()
+        {
+            layout.Children.Add(redBox, Constraint.RelativeToParent((parent) => {
+                return parent.X;
+            }), Constraint.RelativeToParent((parent) => {
+                return parent.Y * .15;
+            }), Constraint.RelativeToParent((parent) => {
+                return parent.Width;
+            }), Constraint.RelativeToParent((parent) => {
+                return parent.Height * .8;
+            }));
+            layout.Children.Add(blueBox, Constraint.RelativeToView(redBox, (Parent, sibling) => {
+                return sibling.X + 20;
+            }), Constraint.RelativeToView(blueBox, (parent, sibling) => {
+                return sibling.Y + 20;
+            }), Constraint.RelativeToParent((parent) => {
+                return parent.Width * .5;
+            }), Constraint.RelativeToParent((parent) => {
+                return parent.Height * .5;
+            }));
+
+        }
+    }
+}
---
+++ Lab_3/Lab_3/ScrollViewDemo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab_3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScrollViewDemo : ContentPage
+    {
+        public ScrollViewDemo()
+        {
+            var scroll = new ScrollView();
+            Content = scroll;
+            var stack = new StackLayout();
+            stack.Children.Add(new BoxView { BackgroundColor = Color.Red, HeightRequest = 600, WidthRequest = 600 });
+            stack.Children.Add(new Entry());
+
+        }
+    }
+}
